7b0dce3836d7c6ddd3c12ed4465b9025ed57ab81
turetske <turetske@users.noreply.github.com>
2025-08-07 21:32:16 +0000

    Deploying to gh-pages from @ ProjectPythia/osdf-cookbook@c0781ccd71ad07ec52fd6bc9c8bc80757906636f 🚀

---
+++ _preview/65/build/_shared/firestore-security-rules-T7OBNEPB.js
@@ -0,0 +1,1 @@
+import{a}from"/osdf-cookbook/_preview/65/build/_shared/chunk-OTTI7VX4.js";import"/osdf-cookbook/_preview/65/build/_shared/chunk-2NH4LW52.js";export default a();
---
+++ _preview/65/build/_shared/typescript-WGVCMFMI.js
@@ -0,0 +1,1 @@
+import{a}from"/osdf-cookbook/_preview/65/build/_shared/chunk-QXPMMO3F.js";import"/osdf-cookbook/_preview/65/build/_shared/chunk-2NH4LW52.js";export default a();
